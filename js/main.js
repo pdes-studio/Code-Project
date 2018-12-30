3474ca84822bdf67cb1c4809b89f151fbb28a32e
pdes-studio <paulo-mega@hotmail.com>
2018-12-30 05:47:00 -0200

    1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".fade_esq").addClass("animated fadeInLeft");
+	}, 400);
+
+setTimeout(function () {
+    $(".fade_dir").addClass("animated fadeInRight");
+  }, 400);
+
+
+  setTimeout(function () {
+  $(".fade_down").addClass("animated fadeInDown");
+  }, 700);
+
+});
